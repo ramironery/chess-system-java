bc8619fa3d2d6e0f194f6995aaa6a761416c5a81
ramironery <ramironery@gmail.com>
2021-04-30 22:58:01 -0300

    Special move castling

--- src/application/UI.java
+++ src/application/UI.java
@@ -70,6 +70,11 @@ public class UI {
 		}
 	}
 
+	private static void printBoard(ChessPiece[][] pieces) {
+		// TODO Auto-generated method stub
+		
+	}
+
 	public static void printBoard(ChessPiece[][] pieces, boolean[][] possibleMoves) {
 		for (int i = 0; i < pieces.length; i++) {
 			System.out.print((8 - i) + " ");
--- src/chess/Color.java
+++ src/chess/Color.java
@@ -1,6 +1,14 @@
 package chess;
 
+import java.util.List;
+import java.util.stream.Collector;
+
 public enum Color {
 	BLACK,
 	WHITE;
+
+	Color collect(Collector<Object, ?, List<Object>> list) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
